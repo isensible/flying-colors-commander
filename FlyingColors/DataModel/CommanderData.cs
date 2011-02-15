1ea82b40342f033410ab8210dad88a06715629f7
ejames78 <ejames78@gmail.com>
2011-02-15 20:04:29 +0000

    Added CommanderData and FleetShipCommanderData

---
+++ FlyingColors/DataModel/CommanderData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Castle.ActiveRecord;
+
+namespace FlyingColors.DataModel
+{
+	[Serializable]
+	[ActiveRecord("Commander")]
+	public class CommanderData
+	{
+		[PrimaryKey]
+		public long CommanderId { get; set; }
+
+		[Property]
+		public string Name { get; set; }
+
+		[Property]
+		public string Nationality { get; set; }
+
+		[Property]
+		public int VictoryPoints { get; set; }
+
+		[Property]
+		public int Rank { get; set; }
+
+		[Property]
+		public int CommandQuality { get; set; }
+
+		[Property]
+		public int CommandRange { get; set; }
+	}
+}
---
+++ FlyingColors/DataModel/FleetShipCommanderData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.ActiveRecord;
+
+namespace FlyingColors.DataModel
+{
+	[Serializable]
+	[ActiveRecord("FleetShipCommander")]
+	public class FleetShipCommanderData
+	{
+		[PrimaryKey]
+		public long FleetShipCommanderId { get; set; }
+
+		[BelongsTo(Type = typeof(FleetShipData), Column = "FleetShipId")]
+		public FleetShipData FleetShip { get; set; }
+
+		[BelongsTo(Type = typeof(CommanderData), Column = "CommanderId")]
+		public CommanderData Commander { get; set; }
+	}
+}
