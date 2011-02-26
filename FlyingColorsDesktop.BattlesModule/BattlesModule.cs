de70111fcf64f90144c8110bc44edbc53d66d041
ejames78 <ejames78@gmail.com>
2011-02-26 22:40:16 +0000

    Second draft of UI has begun...basic Prism framework setup with BattleModule.

---
+++ FlyingColorsDesktop.BattlesModule/BattlesModule.cs
@@ -0,0 +1,21 @@
+﻿using FlyingColorsDesktop.Views;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace FlyingColorsDesktop
+{
+	public class BattlesModule : IModule
+	{
+		private readonly IRegionManager _regionManager;
+
+		public BattlesModule(IRegionManager regionManager)
+		{
+			_regionManager = regionManager;
+		}
+
+		public void Initialize()
+		{
+			_regionManager.RegisterViewWithRegion("MainRegion", typeof(BattlesView));
+		}
+	}
+}
